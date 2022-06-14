e3a631d3d8e9e9700b7e9cfbda4ae10bcbb662e1
Sajadbasim97 <105778858+Sajadbasim97@users.noreply.github.com>
2022-06-14 21:37:09 +0300

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,11 @@
+<?php
+
+#echo "first tutorial";
+$TOKEN = "5542359014:AAGlnzueKrs9yqB7ZHd4Mp_bQ0lqRDWwADw";
+require "function.php";
+
+if($text == "/start"):
+
+    SendMessage($chat_id,"hi");
+
+    endif;
